914db9f873fc71a7d465a41f1c441cc3241beb57
Adarsh3112 <76930096+Adarsh3112@users.noreply.github.com>
2021-01-05 13:25:42 +0530

    Update StackLinkedList.c

--- StackLinkedList.c
+++ StackLinkedList.c
@@ -1,119 +1,121 @@
-#include <stdio.h>  
-#include <stdlib.h>  
-void push();  
-void pop();  
-void display();  
-struct node   
-{  
-int val;  
-struct node *next;  
-};  
-struct node *head;  
-  
-void main ()  
-{  
-    int choice=0;     
-    printf("\n*********Stack operations using linked list*********\n");  
-    printf("\n----------------------------------------------\n");  
-    while(choice != 4)  
-    {  
-        printf("\n\nChose one from the below options...\n");  
-        printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");  
-        printf("\n Enter your choice \n");        
-        scanf("%d",&choice);  
-        switch(choice)  
-        {  
-            case 1:  
-            {   
-                push();  
-                break;  
-            }  
-            case 2:  
-            {  
-                pop();  
-                break;  
-            }  
-            case 3:  
-            {  
-                display();  
-                break;  
-            }  
-            case 4:   
-            {  
-                printf("Exiting....");  
-                break;   
-            }  
-            default:  
-            {  
-                printf("Please Enter valid choice ");  
-            }   
-    };  
-}  
-}  
-void push ()  
-{  
-    int val;  
-    struct node *ptr = (struct node*)malloc(sizeof(struct node));   
-    if(ptr == NULL)  
-    {  
-        printf("not able to push the element");   
-    }  
-    else   
-    {  
-        printf("Enter the value");  
-        scanf("%d",&val);  
-        if(head==NULL)  
-        {         
-            ptr->val = val;  
-            ptr -> next = NULL;  
-            head=ptr;  
-        }   
-        else   
-        {  
-            ptr->val = val;  
-            ptr->next = head;  
-            head=ptr;  
-               
-        }  
-        printf("Item pushed");  
-          
-    }  
-}  
-  
-void pop()  
-{  
-    int item;  
-    struct node *ptr;  
-    if (head == NULL)  
-    {  
-        printf("Underflow");  
-    }  
-    else  
-    {  
-        item = head->val;  
-        ptr = head;  
-        head = head->next;  
-        free(ptr);  
-        printf("Item popped");  
-          
-    }  
-}  
-void display()  
-{  
-    int i;  
-    struct node *ptr;  
-    ptr=head;  
-    if(ptr == NULL)  
-    {  
-        printf("Stack is empty\n");  
-    }  
-    else  
-    {  
-        printf("Printing Stack elements \n");  
-        while(ptr!=NULL)  
-        {  
-            printf("%d\n",ptr->val);  
-            ptr = ptr->next;  
-        }  
-    }  
-}  
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/* Structure of a node */
+struct node {
+    int data;          // Data 
+    struct node *next; // Address 
+}*head;
+
+
+void createList(int n);
+void displayList();
+
+
+int main()
+{
+    int n, data;
+
+    /*
+     * Create a singly linked list of n nodes
+     */
+    printf("Enter the total number of nodes: ");
+    scanf("%d", &n);
+    createList(n);
+
+    printf("\nData in the list \n");
+    displayList();
+
+    return 0;
+}
+
+
+/*
+ * Create a list of n nodes
+ */
+void createList(int n)
+{
+    struct node *newNode, *temp;
+    int data, i;
+
+    head = (struct node *)malloc(sizeof(struct node));
+
+    /*
+     * If unable to allocate memory for head node
+     */
+    if(head == NULL)
+    {
+        printf("Unable to allocate memory.");
+    }
+    else
+    {
+        /*
+         * Input data of node from the user
+         */
+        printf("Enter the data of node 1: ");
+        scanf("%d", &data);
+
+        head->data = data; // Link data field with data
+        head->next = NULL; // Link address field to NULL
+
+        temp = head;
+
+        /*
+         * Create n nodes and adds to linked list
+         */
+        for(i=2; i<=n; i++)
+        {
+            newNode = (struct node *)malloc(sizeof(struct node));
+
+            /* If memory is not allocated for newNode */
+            if(newNode == NULL)
+            {
+                printf("Unable to allocate memory.");
+                break;
+            }
+            else
+            {
+                printf("Enter the data of node %d: ", i);
+                scanf("%d", &data);
+
+                newNode->data = data; // Link data field of newNode with data
+                newNode->next = NULL; // Link address field of newNode with NULL
+
+                temp->next = newNode; // Link previous node i.e. temp to the newNode
+                
+                temp = temp->next; 
+            }
+        }
+
+        printf("SINGLY LINKED LIST CREATED SUCCESSFULLY\n");
+    }
+}
+
+
+
+
+/*
+ * Display entire list
+ */
+void displayList()
+{
+    struct node *temp;
+
+    /*
+     * If the list is empty i.e. head = NULL
+     */
+    if(head == NULL)
+    {
+        printf("List is empty.");
+    }
+    else
+    {
+        temp = head;
+        while(temp != NULL)
+        {
+            printf("Data = %d\n", temp->data); // Print data of current node
+            temp = temp->next;                 // Move to next node
+        }
+    }
+}
